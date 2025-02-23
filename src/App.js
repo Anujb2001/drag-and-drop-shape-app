48a0882c801bd8e7bfbdbe64cc6eec6c8ef34dcd
Anujb2001 <146002516+Anujb2001@users.noreply.github.com>
2025-02-23 08:00:46 +0000

    Added the circle shape to check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ const App = () => {
             <h1> Drag and drop shape application</h1>
             {/* created the containers for holding all four quadrants */}
             <div className="container" >
-                <div>Top Left</div>
+                <div>Top Left
+                    <div className="circle"/>
+                </div>
                 <div>Top Right</div>
                 <div>Bottom Left</div>
                 <div>Bottom Right</div>
