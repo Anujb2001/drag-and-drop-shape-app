947ccb07a287d6c5bbdc7769ee95488c94471ee5
Anujb2001 <146002516+Anujb2001@users.noreply.github.com>
2025-02-23 09:12:03 +0000

    Added the initial shapes data and shape container list to render the container and update the css for style the shape item based on container class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from "react";  
+import React, {useState, useEffect} from "react";  
 import './App.scss'
 
 const App = () => {
+    // consider initial shapes all are circle
+    const initialData = [{index:1, type:"circle"}, {index:2, type:"circle"}]
+    const [shapes, setShapes] = useState(initialData)
+    // Created the list of four quadrant with shape type
+    const shapeContainers = [
+        {name:"Top Left", shapeType:"circle"},
+        {name:"Top Right", shapeType:"hexagon"},
+        {name:"Bottom Left", shapeType:"square"},
+        {name:"Bottom Right", shapeType:"triangle"},
+    ]
+    
+
     return (
         <div>
             <h1> Drag and drop shape application</h1>
             {/* created the containers for holding all four quadrants */}
             <div className="container" >
-                <div>Top Left
-                    <div className="circle"/>
-                </div>
-                <div>Top Right</div>
-                <div>Bottom Left</div>
-                <div>Bottom Right</div>
+                {shapeContainers.map(container => <div className={container.shapeType}> 
+                  {container.name} 
+                  {shapes.filter(s => s.type === container.shapeType).map((s)=>
+                    // Render the shape item 
+                    <div key={s.index}></div>
+                )} 
+                </div>)}
+                
             </div>
         </div>
     )
